71103cf00960c44a781b05b0659afd75502aab96
MarengoAlberto <alberto@bitstrapped.com>
2024-02-18 13:40:00 -0800

    fixed bug in setup package

--- scripts/train_distributed.sh
+++ scripts/train_distributed.sh
@@ -2,7 +2,7 @@
 
 set -a
 source environments/.env
-python distributed_utils/setup.py sdist --formats=gztar
+python setup.py sdist --formats=gztar
 gsutil cp $SOURCE_PACKAGE_FILE_NAME $PYTHON_PACKAGE_GCS_URI
 gsutil ls -l $PYTHON_PACKAGE_GCS_URI
 python distributed_utils/launch_distributed_training_job.py
